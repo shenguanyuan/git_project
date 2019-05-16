39b3c8d685126599805ec8d8c3fdfffec0c2154e
shenguanyuan <1252217234@qq.com>
2019-05-16 17:25:07 +0800

    初始化工程

--- student/src/com/nnxy/model/Student.java
+++ student/src/com/nnxy/model/Student.java
@@ -1,5 +1,45 @@
 package com.nnxy.model;
 
 public class Student {
-
+	
+	private int id;
+	private String name;
+	private int age;
+	private int sex;
+	
+	
+	
+	public Student(int id, String name, int age, int sex) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.sex = sex;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	
 }
